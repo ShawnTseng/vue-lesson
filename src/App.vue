713a36a1c8f78d0d4d283eb834613498d242318b
shawntseng <shawntseng40@gmail.com>
2020-05-05 14:22:26 +0800

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TodoInput @submit="addTodo" />
+    <ul>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :data="todo"
+        @click.native="removeTodo(index)"
+      >{{ todo }}</TodoItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoInput from "./components/TodoInput.vue";
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      todos: []
+    };
+  },
   components: {
-    HelloWorld
+    TodoInput,
+    TodoItem
+  },
+  methods: {
+    addTodo(text) {
+      this.todos.push(text);
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
